e3b014a23ecf958a1e5e0a8a65bcbafe7e9cf999
Hannah Harrington <hannahharrington@outlook.com>
2019-12-21 12:39:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,15 +5,26 @@ def fourth_challenge
        :matriarch => {name: "Lady Montague", age: "54"},
        :hero => {name: "Romeo", age: "15", status: "alive"},
        :hero_friends => [
+<<<<<<< HEAD
          {name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}
+=======
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
+>>>>>>> ca5c7074db5ecb69262040983afd2a44760e199f
        ]
     }, 
     :capulet => {
        :patriarch => {name: "Lord Capulet", age: "50"},
        :matriarch => {name: "Lady Capulet", age: "51"},
        :heroine => {name: "Juliette", age: "15", status: "alive"},
+<<<<<<< HEAD
        :heroine_friends => [{:age=>"30", :attitude=>"confused", :name=>"Steven"}, 
        {:age=>"44", :attitude=>"worried", :name=>"Nurse"}]
+=======
+       :heroine_friends => [{name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+        {name: "Nurse", age: "44", attitude: "worried"}
+       ]
+>>>>>>> ca5c7074db5ecb69262040983afd2a44760e199f
     }
  }
 end
